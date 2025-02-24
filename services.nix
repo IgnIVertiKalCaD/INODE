6c61fefeaf112ac8df7b8157bb3c219843976f07
IgnIVertiKalCaD <idrobot696@gmail.com>
2025-02-25 01:49:35 +0300

    das

--- services.nix
+++ services.nix
@@ -2,8 +2,33 @@
 
 {
   services = {
-    syncthing = { enable = true;};
+    syncthing = { 
+	enable = true;
+	systemService = false;
+	user = "igni";
+    group   = "users";
+	dataDir = "/home/igni";	
+    configDir = "/home/igni/.config/syncthing";
 
+    settings = {
+        gui = {
+        user = "igni-sync";
+         password = "9838de7d-7f02-4046-9f38-2349b8fc61c9";
+          theme = "black";
+        };
+        devices = {
+        	smartphone = {
+        	  id = "7CFNTQM-IMTJBHJ-3UWRDIU-ZGQJFR6-VCXZ3NB-XUH3KZO-N52ITXR-LAIYUAU";
+        	};
+        }; 
+		folders = {
+      			"Documents" = {
+        			path = "/home/igni/Documents";
+      			};
+		};
+ 	};
+    };
+    
     gnome.gnome-keyring.enable = true;
 
     tailscale.enable = true;
